bee5b7531c1554fba17bfd7b5dbf367bbc3cfa47
seonpilKim <ksp970306@gmail.com>
2022-06-30 08:22:52 +0900

    Add: 김선필.cpp
    Greedy 테크닉과 Disjoint-set 자료구조를 이용하여 해결

---
+++ BOJ/공항/김선필.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> par;
+
+int find(int u) {
+	return (u == par[u] ? u : par[u] = find(par[u]));
+}
+
+void merge(int u, int v) {
+	u = find(u);
+	v = find(v);
+
+	if (u == v) {
+		u = find(--u);
+	}
+
+	par[v] = u;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int G, P;
+	cin >> G >> P;
+	
+	par.resize(G + 1);
+	for (int i = 1; i <= G; i++) {
+		par[i] = i;
+	}
+
+	vector<int> g(P);
+	for (int i = 0; i < P; i++) {
+		cin >> g[i];
+	}
+
+	int count = 0;
+	for (int i = 0; i < P; i++) {
+		if (find(g[i]) == 0) {
+			break;
+		}
+
+		count++;
+		merge(g[i] - 1, g[i]);
+	}
+
+	cout << count;
+
+	return 0;
+}
