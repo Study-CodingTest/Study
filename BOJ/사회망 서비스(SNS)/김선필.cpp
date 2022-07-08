37812329441c1f36fc9feac40d2dcfb8d10d8c2b
seonpilKim <ksp970306@gmail.com>
2022-07-08 22:23:06 +0900

    Refactor: 메모이제이션 제거

--- BOJ/사회망 서비스(SNS)/김선필.cpp
+++ BOJ/사회망 서비스(SNS)/김선필.cpp
@@ -6,19 +6,22 @@ using namespace std;
 const int NORMAL = 0;
 const int ADAPTOR = 1;
 
-void dfs(const vector<vector<int>> &adj, vector<vector<int>> &dp, vector<bool> &visit, int cur) {
+vector<int> dfs(const vector<vector<int>> &adj, vector<bool> &visit, int cur) {
+    vector<int> res(2);
+    res[ADAPTOR] = 1;
     visit[cur] = true;
-    dp[cur][ADAPTOR] = 1;
 
     for (auto next : adj[cur]) {
         if (visit[next]) {
             continue;
         }
 
-        dfs(adj, dp, visit, next);
-        dp[cur][NORMAL] += dp[next][ADAPTOR];
-        dp[cur][ADAPTOR] += min(dp[next][NORMAL], dp[next][ADAPTOR]);
+        const vector<int> child = dfs(adj, visit, next);
+        res[NORMAL] += child[ADAPTOR];
+        res[ADAPTOR] += min(child[NORMAL], child[ADAPTOR]);
     }
+
+    return res;
 }
 
 int main() {
@@ -30,7 +33,6 @@ int main() {
 
     vector<bool> visit(N + 1);
     vector<vector<int>> adj(N + 1, vector<int>());
-    vector<vector<int>> dp(N + 1, vector<int>(2));
     for (int i = 1; i < N; i++) {
         int u, v;
         cin >> u >> v;
@@ -38,8 +40,8 @@ int main() {
         adj[v].push_back(u);
     }
 
-    dfs(adj, dp, visit, 1);
-    cout << min(dp[1][ADAPTOR], dp[1][NORMAL]);
+    const vector<int> ans = dfs(adj, visit, 1);
+    cout << min(ans[ADAPTOR], ans[NORMAL]);
 
     return 0;
 }
