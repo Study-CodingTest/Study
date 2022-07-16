b7213e4ef377a711c84f6fdbce90de4750cd4b6a
seonpilKim <ksp970306@gmail.com>
2022-07-16 23:35:21 +0900

    Add: 김선필.cpp
    구현

---
+++ BOJ/나무 위의 빗물/김선필.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, W;
+    cin >> N >> W;
+    vector<int> adj(N + 1);
+
+    for (int i = 1; i < N; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u]++;
+        adj[v]++;
+    }
+
+    int leaves = 0;
+    for (int i = 2; i <= N; i++) {
+        if (adj[i] == 1) {
+            leaves++;
+        }
+    }
+
+    cout.precision(11);
+    cout << (double)W / leaves;
+
+    return 0;
+}
